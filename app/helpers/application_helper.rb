94628616526f80467ca24359945da5f08602b8f3
Katie Bridson <kbridson@memphis.edu>
2026-03-30 20:34:17 -0500

    Added Bootstrap navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,12 @@ module ApplicationHelper
     BOOTSTRAP_ALERT_CLASS.fetch(flash_key, 'alert-info')
   end
 
+  def active_class(path)
+    if request.path == path
+      'active'
+    else
+      ''
+    end
+  end
+
 end
